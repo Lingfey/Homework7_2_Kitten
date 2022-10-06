c9f86b4520a1d88b1bec250b338a5220ce73f234
Lingfey <bokanchalangman@gmail.com>
2022-10-07 03:18:52 +0800

    Kitten
    Introduction to classes

---
+++ Homework7_2_Kitten/Kitten.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Homework7_2_Kitten
+{
+    public class Kitten
+    {
+        public string Name { get; set; }
+        public string FurColor { get; set; }
+        public string EyeColor { get; set; }
+        public int Age { get; set; }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nAge: {Age} y.o.\nFur Color: {FurColor}\n" +
+                $"Eye Color: {EyeColor}");
+        }
+
+        public void Speak(int times)
+        {
+            Console.WriteLine($"{Name} says: " + String.Concat(Enumerable.Repeat("Meow ", times)));
+        }
+    }
+}
+
